c8d88afe860bdb301004e103ec36d2ba93ec0d8b
jim.yu <jimyu1994@gmail.com>
2020-03-07 05:54:44 -0800

    orignaize and add logging

--- client/client.py
+++ client/client.py
@@ -5,35 +5,28 @@ import numpy as np
 import cv2
 
 addr = 'http://localhost:5001'
-test_url = addr + '/api/flip'
+flip_url = addr + '/api/flip'
 
 # prepare headers for http request
 content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 
 print("reading image now")
-
 img = cv2.imread('nice.jpg')
+
 # encode image as jpeg
+print("encoding image")
 _, img_encoded = cv2.imencode('.jpeg', img)
-print("sending data out")
+
 # send http request with image and receive response
-response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
-# decode response
-# convert string of image data to uint8
-# print(response.text)
-# jsonTree = json.loads(response.text)
-# imjson = str(jsonTree['data'])
-print(response.content)
+print("sending data out and get response back")
+response = requests.post(flip_url, data=img_encoded.tostring(), headers=headers)
+
+print("decoding data into images")
+# convert content of response data to uint8
 nparr = np.fromstring(response.content, np.uint8)
 # # decode image
 img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-# cv2.imwrite("fliped.jpg", img)
-# nparr = np.fromstring(json.loads(response.text), np.uint8)
-# print(nparr)
-# # decode image
-# img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+print("write respond image into cache for further use")
 cv2.imwrite("edited/fliped.jpg", img)
-
-# expected output: {u'message': u'image received. size=124x124'}
--- server/server.py
+++ server/server.py
@@ -11,30 +11,20 @@ app = Flask(__name__)
 @app.route('/api/flip', methods=['POST'])
 def test():	
     r = request
-    print("get the request!")
+    print("get the request! starting decode image from request message")
     # convert string of image data to uint8
     nparr = np.fromstring(r.data, np.uint8)
     # decode image
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # do some fancy processing here....
-    print("fancy stuffs")
+    # do your image process logic here....
+    print("fliping yo yo yo")
     flip = cv2.flip(img, 0)
+
+    print("finish fliping, encode for transmission")
     _, img_encoded = cv2.imencode('.jpeg', flip)
 
-    # # _, img_encoded = cv2.imencode('.jpeg', img)
-    # # print("saving a fliped image")
-    # # Create and add usertoek in the path if needed
-    # # cv2.imwrite("tmp/fliped.jpg", flip
-
-    # # build a response dict to send back to client
-    # response = {'data': img_encoded.tostring()}
-    # # encode response using jsonpickle
-    # response_pickled = jsonpickle.encode(response)
-
-    # print("respoding")
-    # # return send_file()
-    print(img_encoded.tostring())
+    print("all set, responding with image binary as string")
     return img_encoded.tostring(), 200
 
 
